83c82c1feca5356a12234358275d1206cdca1e24
Steven Lageveen <stevenmax888@gmail.com>
2024-07-08 16:22:50 +0200

    Removed decorations aside from the borders

--- src/deco-subsurface.cpp
+++ src/deco-subsurface.cpp
@@ -19,7 +19,6 @@
 #include <wayfire/signal-definitions.hpp>
 #include <wayfire/toplevel-view.hpp>
 #include "deco-subsurface.hpp"
-#include "deco-layout.hpp"
 #include "deco-theme.hpp"
 #include <wayfire/window-manager.hpp>
 
@@ -29,9 +28,7 @@
 
 namespace wb
 {
-class simple_decoration_node_t : public wf::scene::node_t,
-				 public wf::pointer_interaction_t,
-				 public wf::touch_interaction_t {
+class simple_decoration_node_t : public wf::scene::node_t {
 	std::weak_ptr<wf::toplevel_view_interface_t> _view;
 	wf::signal::connection_t<wf::view_title_changed_signal> title_set =
 		[=](wf::view_title_changed_signal *ev) {
@@ -40,25 +37,6 @@ class simple_decoration_node_t : public wf::scene::node_t,
 			}
 		};
 
-	void update_title(int width, int height, double scale)
-	{
-		if (auto view = _view.lock()) {
-			int target_width = width * scale;
-			int target_height = height * scale;
-			if ((title_texture.tex.width != target_width) ||
-			    (title_texture.tex.height != target_height) ||
-			    (title_texture.current_text != view->get_title())) {
-				auto surface = theme.render_text(
-					view->get_title(), target_width,
-					target_height);
-				cairo_surface_upload_to_texture(
-					surface, title_texture.tex);
-				cairo_surface_destroy(surface);
-				title_texture.current_text = view->get_title();
-			}
-		}
-	}
-
 	struct {
 		wf::simple_texture_t tex;
 		std::string current_text = "";
@@ -66,8 +44,6 @@ class simple_decoration_node_t : public wf::scene::node_t,
 
     public:
 	wb::decor::decoration_theme_t theme;
-	wb::decor::decoration_layout_t layout;
-	wf::region_t cached_region;
 
 	wf::dimensions_t size;
 
@@ -77,23 +53,9 @@ class simple_decoration_node_t : public wf::scene::node_t,
 	simple_decoration_node_t(wayfire_toplevel_view view)
 		: node_t(false)
 		, theme{}
-		, layout{ theme, [=](wlr_box box) {
-				 wf::scene::damage_node(shared_from_this(),
-							box + get_offset());
-			 } }
 	{
 		this->_view = view->weak_from_this();
 		view->connect(&title_set);
-		if (view->parent) {
-			theme.set_buttons(wb::decor::button_type_t(
-				wb::decor::BUTTON_TOGGLE_MAXIMIZE |
-				wb::decor::BUTTON_CLOSE));
-		} else {
-			theme.set_buttons(wb::decor::button_type_t(
-				wb::decor::BUTTON_MINIMIZE |
-				wb::decor::BUTTON_TOGGLE_MAXIMIZE |
-				wb::decor::BUTTON_CLOSE));
-		}
 
 		// make sure to hide frame if the view is fullscreen
 		update_decoration_size();
@@ -101,16 +63,7 @@ class simple_decoration_node_t : public wf::scene::node_t,
 
 	wf::point_t get_offset()
 	{
-		return { -current_thickness, -current_titlebar };
-	}
-
-	void render_title(const wf::render_target_t &fb,
-			  wf::geometry_t geometry)
-	{
-		update_title(geometry.width, geometry.height, fb.scale);
-		OpenGL::render_texture(title_texture.tex.tex, fb, geometry,
-				       glm::vec4(1.0f),
-				       OpenGL::TEXTURE_TRANSFORM_INVERT_Y);
+		return { -current_thickness, -current_thickness };
 	}
 
 	void render_scissor_box(const wf::render_target_t &fb,
@@ -125,50 +78,6 @@ class simple_decoration_node_t : public wf::scene::node_t,
 		}
 
 		theme.render_background(fb, geometry, scissor, activated);
-
-		/* Draw title & buttons */
-		auto renderables = layout.get_renderable_areas();
-		for (auto item : renderables) {
-			if (item->get_type() ==
-			    wb::decor::DECORATION_AREA_TITLE) {
-				OpenGL::render_begin(fb);
-				fb.logic_scissor(scissor);
-				render_title(fb, item->get_geometry() + origin);
-				OpenGL::render_end();
-			} else // button
-			{
-				item->as_button().render(
-					fb, item->get_geometry() + origin,
-					scissor);
-			}
-		}
-	}
-
-	std::optional<wf::scene::input_node_t>
-	find_node_at(const wf::pointf_t &at) override
-	{
-		if (auto view = _view.lock()) {
-			wf::pointf_t local = at - wf::pointf_t{ get_offset() };
-			if (cached_region.contains_pointf(local) &&
-			    view->is_mapped()) {
-				return wf::scene::input_node_t{
-					.node = this,
-					.local_coords = local,
-				};
-			}
-		}
-
-		return {};
-	}
-
-	pointer_interaction_t &pointer_interaction() override
-	{
-		return *this;
-	}
-
-	touch_interaction_t &touch_interaction() override
-	{
-		return *this;
 	}
 
 	class decoration_render_instance_t
@@ -200,17 +109,6 @@ class simple_decoration_node_t : public wf::scene::node_t,
 			const wf::render_target_t &target,
 			wf::region_t &damage) override
 		{
-			auto our_region =
-				self->cached_region + self->get_offset();
-			wf::region_t our_damage = damage & our_region;
-			if (!our_damage.empty()) {
-				instructions.push_back(
-					wf::scene::render_instruction_t{
-						.instance = this,
-						.target = target,
-						.damage = std::move(our_damage),
-					});
-			}
 		}
 
 		void render(const wf::render_target_t &target,
@@ -239,107 +137,11 @@ class simple_decoration_node_t : public wf::scene::node_t,
 		return wf::construct_box(get_offset(), size);
 	}
 
-	/* wf::compositor_surface_t implementation */
-	void handle_pointer_enter(wf::pointf_t point) override
-	{
-		point -= wf::pointf_t{ get_offset() };
-		layout.handle_motion(point.x, point.y);
-	}
-
-	void handle_pointer_leave() override
-	{
-		layout.handle_focus_lost();
-	}
-
-	void handle_pointer_motion(wf::pointf_t to, uint32_t) override
-	{
-		to -= wf::pointf_t{ get_offset() };
-		handle_action(layout.handle_motion(to.x, to.y));
-	}
-
-	void handle_pointer_button(const wlr_pointer_button_event &ev) override
-	{
-		if (ev.button != BTN_LEFT) {
-			return;
-		}
-
-		handle_action(layout.handle_press_event(ev.state ==
-							WLR_BUTTON_PRESSED));
-	}
-
-	void
-	handle_action(wb::decor::decoration_layout_t::action_response_t action)
-	{
-		if (auto view = _view.lock()) {
-			switch (action.action) {
-			case wb::decor::DECORATION_ACTION_MOVE:
-				return wf::get_core().default_wm->move_request(
-					view);
-
-			case wb::decor::DECORATION_ACTION_RESIZE:
-				return wf::get_core().default_wm->resize_request(
-					view, action.edges);
-
-			case wb::decor::DECORATION_ACTION_CLOSE:
-				return view->close();
-
-			case wb::decor::DECORATION_ACTION_TOGGLE_MAXIMIZE:
-				if (view->pending_tiled_edges()) {
-					return wf::get_core()
-						.default_wm->tile_request(view,
-									  0);
-				} else {
-					return wf::get_core()
-						.default_wm->tile_request(
-							view,
-							wf::TILED_EDGES_ALL);
-				}
-
-				break;
-
-			case wb::decor::DECORATION_ACTION_MINIMIZE:
-				return wf::get_core()
-					.default_wm->minimize_request(view,
-								      true);
-				break;
-
-			default:
-				break;
-			}
-		}
-	}
-
-	void handle_touch_down(uint32_t time_ms, int finger_id,
-			       wf::pointf_t position) override
-	{
-		handle_touch_motion(time_ms, finger_id, position);
-		handle_action(layout.handle_press_event());
-	}
-
-	void handle_touch_up(uint32_t time_ms, int finger_id,
-			     wf::pointf_t lift_off_position) override
-	{
-		handle_action(layout.handle_press_event(false));
-		layout.handle_focus_lost();
-	}
-
-	void handle_touch_motion(uint32_t time_ms, int finger_id,
-				 wf::pointf_t position) override
-	{
-		position -= wf::pointf_t{ get_offset() };
-		layout.handle_motion(position.x, position.y);
-	}
-
 	void resize(wf::dimensions_t dims)
 	{
 		if (auto view = _view.lock()) {
 			view->damage();
 			size = dims;
-			layout.resize(size.width, size.height);
-			if (!view->toplevel()->current().fullscreen) {
-				this->cached_region = layout.calculate_region();
-			}
-
 			view->damage();
 		}
 	}
@@ -350,13 +152,8 @@ class simple_decoration_node_t : public wf::scene::node_t,
 			_view.lock()->toplevel()->current().fullscreen;
 		if (fullscreen) {
 			current_thickness = 0;
-			current_titlebar = 0;
-			this->cached_region.clear();
 		} else {
 			current_thickness = theme.get_border_size();
-			current_titlebar = theme.get_title_height() +
-					   theme.get_border_size();
-			this->cached_region = layout.calculate_region();
 		}
 	}
 };
@@ -402,13 +199,11 @@ wb::simple_decorator_t::get_margins(const wf::toplevel_state_t &state)
 	}
 
 	const int thickness = deco->theme.get_border_size();
-	const int titlebar =
-		deco->theme.get_title_height() + deco->theme.get_border_size();
 	return wf::decoration_margins_t{
 		.left = thickness,
 		.right = thickness,
 		.bottom = thickness,
-		.top = titlebar,
+		.top = thickness,
 	};
 }
 }
--- src/deco-theme.hpp
+++ src/deco-theme.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <wayfire/render-manager.hpp>
-#include "deco-button.hpp"
 
 namespace wb
 {
@@ -15,13 +14,8 @@ class decoration_theme_t {
 	/** Create a new theme with the default parameters */
 	decoration_theme_t();
 
-	/** @return The available height for displaying the title */
-	int get_title_height() const;
 	/** @return The available border for resizing */
 	int get_border_size() const;
-	/** Set the flags for buttons */
-	void set_buttons(button_type_t flags);
-	button_type_t button_flags;
 
 	/**
      * Fill the given rectangle with the background color(s).
@@ -35,35 +29,6 @@ class decoration_theme_t {
 			       wf::geometry_t rectangle,
 			       const wf::geometry_t &scissor,
 			       bool active) const;
-
-	/**
-     * Render the given text on a cairo_surface_t with the given size.
-     * The caller is responsible for freeing the memory afterwards.
-     */
-	cairo_surface_t *render_text(std::string text, int width,
-				     int height) const;
-
-	struct button_state_t {
-		/** Button width */
-		double width;
-		/** Button height */
-		double height;
-		/** Button outline size */
-		double border;
-		/** Progress of button hover, in range [-1, 1].
-         * Negative numbers are usually used for pressed state. */
-		double hover_progress;
-	};
-
-	/**
-     * Get the icon for the given button.
-     * The caller is responsible for freeing the memory afterwards.
-     *
-     * @param button The button type.
-     * @param state The button state.
-     */
-	cairo_surface_t *get_button_surface(button_type_t button,
-					    const button_state_t &state) const;
 
     private:
 	wf::option_wrapper_t<std::string> font{ "borders/font" };
